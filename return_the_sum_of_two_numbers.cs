eb38dd3b308dd6728c460e6d1cb9e4b4eacbdcc7
scott-demmert <73268848+scott-demmert@users.noreply.github.com>
2022-08-13 08:32:22 -0400

    Create return_the_sum_of_two_numbers.cs

---
+++ return_the_sum_of_two_numbers.cs
@@ -0,0 +1,24 @@
+/* LINK: https://edabit.com/challenge/xfRucdwGksiyjZq4K
+
+Return the Sum of Two Numbers
+
+Create a function that takes two numbers as arguments and returns their sum.
+Examples
+
+Sum(3, 2) ➞ 5
+
+Sum(-3, -6) ➞ -9
+
+Sum(7, 3) ➞ 10
+
+Notes
+
+    Don't forget to return the result.
+    If you get stuck on a challenge, find help in the Resources tab.
+    If you're really stuck, unlock solutions in the Solutions tab. */
+
+public class Program
+{
+    // Simple sum can use a lambda expression
+    public static int Sum(int a, int b) => a + b;
+};
