5ceb47bc884cbaf3b2829cbfeece9cd07bb1968e
scott-demmert <73268848+scott-demmert@users.noreply.github.com>
2022-08-13 08:44:36 -0400

    Create correct_the_mistakes.cs

---
+++ correct_the_mistakes.cs
@@ -0,0 +1,26 @@
+/* LINK: https://edabit.com/challenge/dWoZAR2iXWqn8ieCa
+
+Correct the Mistakes
+
+Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+Examples
+
+Squared(5) ➞ 25
+
+Squared(9) ➞ 81
+
+Squared(100) ➞ 10000
+
+Notes
+
+    READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+    Don't overthink this challenge; it's not supposed to be hard. */
+
+public class Program
+{
+	// Method name syntax error and variable reference error - gets the best of us.
+	public static int Squared(int a)
+	{
+		return a * a;
+	}
+}
