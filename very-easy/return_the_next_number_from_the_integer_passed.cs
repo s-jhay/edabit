02ca6cd755b3bd0dfc5fdcbc749c0372389a0467
scott-demmert <73268848+scott-demmert@users.noreply.github.com>
2022-08-20 09:46:11 -0400

    Create return_the_next_number_from_the_integer_passed.cs

---
+++ very-easy/return_the_next_number_from_the_integer_passed.cs
@@ -0,0 +1,26 @@
+/* LINK: https://edabit.com/challenge/RzkLShpDgDqG3c45H
+
+Return the Next Number from the Integer Passed
+
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+Examples
+
+Addition(0) ➞ 1
+
+Addition(9) ➞ 10
+
+Addition(-3) ➞ -2
+
+Notes
+
+    Don't forget to return the result.
+    If you get stuck on a challenge, find help in the Resources tab.
+    If you're really stuck, unlock solutions in the Solutions tab. */
+    
+public class Program
+{
+    public static int Addition(int num)
+    {
+			return num + 1;
+    }
+}
